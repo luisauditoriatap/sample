8b237751e897e89b3f4f2628a7fe35f6f214f793
luisauditoriatap <luis.ortiz.0ds01@utap.edu.com>
2020-08-29 15:57:34 -0500

    Líneas nuevas"

---
+++ src/main.js
@@ -0,0 +1,31 @@
+//input forms
+let addNoteBtn = document.querySelector('#addNoteBtn');
+
+addNoteBtn.onclick = () => {
+
+    let newNoteInput = document.querySelector('#newNoteInput');
+    let notes = document.querySelector('#app .notes');
+    //new elements
+    let note = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.onclick = function () {
+
+        let.parent = this.parentElement;
+        if (this.checked) {
+            parent.classList.add('isDone');
+        }
+
+        else {
+            parent.classList.remove('isDone');
+        }
+
+    }
+
+    note.classList.add('note');
+    note.innerHTML = `<div>${newNoteInput.value}</div>`
+    //note.innerHTML = newNoteInput.value;
+    note.appendChild(checkbox);
+    newNoteInput.value = '';
+    notes.appendChild(note);
+}
